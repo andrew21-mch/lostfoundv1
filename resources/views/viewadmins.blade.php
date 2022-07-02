a20309533b02e82360f55a700d433e5fabd1276d
andrew21-mch <nfonandrew73@gmail.com>
2022-07-02 11:01:43 +0100

    buttons width

--- resources/views/viewadmins.blade.php
+++ resources/views/viewadmins.blade.php
@@ -34,8 +34,8 @@
                             @if($item->role == 'school')
                         School President @else General Admin @endif</td>
                         <td>
-                            <a href="/admin/editadmin/{{$item->id}}" class="btn btn-primary w-50">View</a> 
-                            <a href="/admin/deleteadmin/{{$item->id}}" class="btn btn-danger w-50 ">Delete</@auth
+                            <a href="/admin/editadmin/{{$item->id}}" class="btn btn-primary col-md-4">View</a> 
+                            <a href="/admin/deleteadmin/{{$item->id}}" class="btn btn-danger col-md-4 ">Delete</@auth
                                 
                             @endauth>  
                         </td>
