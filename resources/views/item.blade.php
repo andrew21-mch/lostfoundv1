9036c5f4d7b54844f9fc4c6b5e15f45299e2f761
andrew21-mch <nfonandrew73@gmail.com>
2022-07-18 19:03:25 +0100

    trying to get a particular variable contact

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,17 +1,1 @@
-@extends('layouts.nav')
-@section('content')
-        <div class="row m-2 col-md-8 m-auto">
-            <h2>{{$itemd[0]->itemname}}:{{$itemd[0]->itemid}} Details</h2><hr>
-            <div class="row col-md-4 m-1">
-                <h3 class="m-3">{{$itemd[0]->itemname}}</h3>
-                <p> School: {{$itemd[0]->schoolname}} <br> 
-                    Description: {{$itemd[0]->description}} <br>
-                    <a href='http://wa.me/{{$itemd[0]->contact}}' class="btn btn-success col-md-5 m-1" style="a:hover{color: white; background-color:yellow}">{{$itemd[0]->phone}}</a> </a>
-                </p>
-
-            </div>
-            <div class="row col-md-6 m-1">
-                <img src="../images/{{$itemd[0]->image_url}}" style="height: 70%">
-            </div>
-        </div>
-@endsection
+{{$itemd[0]}}
